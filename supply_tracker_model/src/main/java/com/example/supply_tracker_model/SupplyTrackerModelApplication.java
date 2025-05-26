a573d34e2e9ef5f927c21403e3a7c3dfd23e81eb
Aadarsh Bansal <ab6582@srmist.edu.in>
2025-05-26 15:00:00 +0530

    Initial Setup and boiler plate code

---
+++ supply_tracker_model/src/main/java/com/example/supply_tracker_model/SupplyTrackerModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.supply_tracker_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyTrackerModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyTrackerModelApplication.class, args);
+	}
+
+}
---
+++ supply_tracker_model/src/test/java/com/example/supply_tracker_model/SupplyTrackerModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supply_tracker_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyTrackerModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
