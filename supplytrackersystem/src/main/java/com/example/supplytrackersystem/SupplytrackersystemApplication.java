036aa605c91ec2faf7f68c4c7ec2620c264d826e
deepikaa08 <deepikaagopalakannan@gmail.com>
2025-05-26 14:37:09 +0530

    Initial Setup and BoilerPlate code setup

---
+++ supplytrackersystem/src/main/java/com/example/supplytrackersystem/SupplytrackersystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.supplytrackersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplytrackersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplytrackersystemApplication.class, args);
+	}
+
+}
---
+++ supplytrackersystem/src/test/java/com/example/supplytrackersystem/SupplytrackersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supplytrackersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplytrackersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
